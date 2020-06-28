9017ea597ffe50fa65262816a860235647a8dcee
Neo <48732005+Neokoldstudio@users.noreply.github.com>
2020-06-28 22:29:01 +0200

    Co-authored-by: rems64 <rems64des@gmail.com> css-and-part-of-html-by: Neo <p.godbert@laposte.net>

--- popup.js
+++ popup.js
@@ -4,5 +4,55 @@
 //###############################
 //###############################
 
+$( "#slider-range" ).slider({
+    range: true,
+    min: 0,
+    max: 500,
+    values: [ 0, 500 ],
+    slide: function(event, ui) {
+        var time = [
+            getStringFromTime(getFormattedTimeFromSeconds(ui.values[ 0 ])),
+            getStringFromTime(getFormattedTimeFromSeconds(ui.values[ 1 ]))
+        ]
+        $("#slider-infos")[0].innerHTML =  "De " + time[ 0 ] + " à " + time[ 1 ];
+    }
+});
+
+var time = [
+    getStringFromTime(getFormattedTimeFromSeconds($( "#slider-range" ).slider( "values", 0 ))),
+    getStringFromTime(getFormattedTimeFromSeconds($( "#slider-range" ).slider( "values", 1 )))
+]
+$("#slider-infos")[0].innerHTML = "De " + time[0] + " à " + time[1];
+
 $("#dl-video").on("click", function(event) { // Event triggered when the download button is clicked. event variable contain the event informations
 })
+
+function getFormattedTimeFromSeconds(secs)
+{
+    var hours, minutes, seconds = 0;
+    var isHours = secs / 3600;
+    var isMinutes = secs / 60;
+    var remainder = secs;
+    if(isHours>1)
+    {
+        remainder = remainder%3600;
+        hours = Math.floor(isHours);
+        isMinutes = remainder / 60;
+    }
+    if(isMinutes>1)
+    {
+        remainder = remainder % 60;
+        minutes = Math.floor(isMinutes);
+        seconds = remainder;
+    }
+    return {
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+    };
+}
+
+function getStringFromTime(time)
+{
+    return time.hours + "h " + time.minutes + "min " + time.seconds + "sec";
+}
