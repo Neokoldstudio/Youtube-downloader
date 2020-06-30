14f0faa9206f5084009a48c33a142db930fba17b
Neo <48732005+Neokoldstudio@users.noreply.github.com>
2020-06-30 19:38:38 +0200

    delete the python script that made no sense with what were trying to do

--- download.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-import youtube_dl
-import os
-
-
-ydl_opts = {}
-os.chdir('C:/Users/utilisateur/Desktop')
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=bU5HgCaLU4Q'])
